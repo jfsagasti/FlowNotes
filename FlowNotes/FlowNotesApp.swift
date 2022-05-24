3ab4f1fba3bfe8b6656db7fb5dfc6983122ab857
Juan Sagasti <juan@theagilemonkeys.com>
2022-05-24 12:23:13 +0100

    Initial Commit

---
+++ FlowNotes/FlowNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowNotesApp.swift
+//  FlowNotes
+//
+//  Created by Juan Sagasti on 24/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FlowNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
